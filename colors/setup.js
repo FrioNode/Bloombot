f47d414124cac282df2ab9cc57645a78392aff0e
frionode <frionode@gmail.com>
2025-12-08 12:14:16 +0300

    Luna base display adapter

--- colors/setup.js
+++ colors/setup.js
@@ -31,7 +31,7 @@ function getAll() {
         channelid: process.env.CHANNELID || '120363321675231023@newsletter',
         channel: process.env.CHANNEL || 'https://whatsapp.com/channel/0029VagLDl6BFLgUIWV9aV2d',
         botname: process.env.BOTNAME || 'Luna',
-        image: process.env.IMAGE || 'https://raw.githubusercontent.com/FrioNode/Bloombot/main/colors/bloom.jpg',
+        image: process.env.IMAGE || 'https://raw.githubusercontent.com/FrioNode/Bloombot/main/colors/luna.png',
         lang: process.env.LANG || 'EN',
         react: process.env.REACT === 'true',
         emoji: process.env.EMOJI || '🌼',
